f52452f7e53c6d00267b86a145607ca3edea006b
John Swanson <john@red.yellow.blue>
2016-03-10 21:03:55 -0500

    Cleanup Files from V1
    removed old files

--- v1 Copy these files To public_html/logout.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$past = time() - 100; 
-//this makes the time in the past to destroy the cookie 
-setcookie(ID_my_site, gone, $past); 
-setcookie(Key_my_site, gone, $past); 
-
-echo'You are Logged Out <a href="">OK</a>';
-echo'<script>location.replace(location.pathname);</script>';
-?>
